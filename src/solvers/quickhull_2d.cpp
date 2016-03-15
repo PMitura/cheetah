2c250ad148ae0496b6568acbf57e4c616828c4be
Peter <pmitura.jr@gmail.com>
2016-03-15 15:01:52 +0100

    Quickhull: recursion outlines

--- src/solvers/quickhull_2d.cpp
+++ src/solvers/quickhull_2d.cpp
@@ -3,14 +3,30 @@
 namespace ch
 {
 
+Quickhull2D::Quickhull2D()
+    :globOut_(NULL)
+{}
+
 Points2D& Quickhull2D::solve(const Points2D& input, Points2D& output)
 {
     return solveNaive(input, output);
 }
 
+void Quickhull2D::recNaive(point_t& a, point_t& b, data_t* plane)
+{
+    if ((*plane).size() == 0)
+        return;
+
+    // find point furthest from ab
+    for (auto& pt : (*plane)) {
+
+    }
+}
+
 Points2D& Quickhull2D::solveNaive(const Points2D& input, Points2D& output)
 {
-
+    // find min and max X
+    
 }
 
 }
--- src/solvers/quickhull_2d.h
+++ src/solvers/quickhull_2d.h
@@ -9,10 +9,14 @@ namespace ch {
 class Quickhull2D : public Solver2D
 {
     public:
+        Quickhull2D();
         Points2D& solve(const Points2D& input, Points2D& output);
 
     private:
+        void recNaive(point_t& a, point_t& b, data_t* plane);
         Points2D& solveNaive(const Points2D& input, Points2D& output);
+
+        Points2D* globOut_;
 };
 
 }
